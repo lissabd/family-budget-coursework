aba6ddcef981f6c360cb538baea00728eaa52c79
Alisa Abdeeva <lisssabd@gmail.com>
2025-05-17 17:35:35 +0300

    fix[backend]: fixed cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,10 +19,15 @@ app = FastAPI(
     openapi_url="/openapi.json",
 )
 
-# CORS: разрешаем браузеру любые запросы
+
+origins = [
+    "http://localhost:3000",  # frontend origin
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,          
     allow_methods=["*"],
     allow_headers=["*"],
 )
